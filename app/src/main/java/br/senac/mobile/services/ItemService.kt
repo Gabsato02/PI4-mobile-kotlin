3bc517731c959ac45c82a32a3c2fb08e23a7c0a1
Gabriel Sato <gabsato02@gmail.com>
2021-11-10 23:10:58 -0300

    Adiciona a busca

--- app/src/main/java/br/senac/mobile/services/ItemService.kt
+++ app/src/main/java/br/senac/mobile/services/ItemService.kt
@@ -4,8 +4,12 @@ import br.senac.mobile.models.Item
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ItemService {
     @GET("item/list/{itemId}")
     fun getItem(@Path("itemId") itemId: Int): Call<Item>
+
+    @GET("item/list")
+    fun getItems(@Query("search") search: String): Call<List<Item>>
 }
